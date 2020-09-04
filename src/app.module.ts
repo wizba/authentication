b9847dc385542b9269e46fa05290526d761415f6
william <williamrabs@gmail.com>
2020-09-04 16:06:39 +0200

    module added

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,13 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CourseController } from './courses/course.controller';
+import { CourseModule } from './courses/course.module';
 
 @Module({
   imports: [
     AuthModule,
-    MongooseModule.forRoot('mongodb://localhost/Fatso')
+    MongooseModule.forRoot('mongodb://localhost/Fatso'),
+    CourseModule
   ],
   controllers: [AppController,CourseController],
   providers: [AppService],
